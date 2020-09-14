48942101980444941a37670667feeca3ab9bc71f
Tropical23 <silope0302@gmail.com>
2020-09-14 13:32:40 +0200

    made authentication for new users

---
+++ src/api/user.js
@@ -0,0 +1,35 @@
+import { basePath, apiVersion } from "./config";
+
+export function signUpApi(data) {
+  const url = `${basePath}/${apiVersion}/sign-up`;
+  const params = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  console.log(data);
+
+  return fetch(url, params)
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((result) => {
+      if (result.user) {
+        return { ok: true, message: "Usuario creado correctamente" };
+      }
+      return {
+        ok: false,
+        message: result.message,
+      };
+    })
+    .catch((err) => {
+      return {
+        ok: false,
+        message: err.message,
+      };
+    });
+}
--- src/pages/Admin/SignIn/SignIn.js
+++ src/pages/Admin/SignIn/SignIn.js
@@ -1,23 +1,31 @@
 import React from "react";
 import { Layout, Tabs } from "antd";
-import { Redirect } from "react-router-dom";
+//import { Redirect } from "react-router-dom";
 import Logo from "../../../assets/img/png/logo.png";
+
+import RegisterForm from "../../Admin/RegisterForm";
 
 import "./SignIn.scss";
 
 export default function SignIn() {
   const { Content } = Layout;
-  const { TabPanel } = Tabs;
+  const { TabPane } = Tabs;
   return (
     <Layout className="sign-in">
       <Content className="sign-in__content">
         <h1 className="sign-in__content-logo">
-          <img
-            src={Logo}
-            className="sign-in__content-logo-img"
-            alt="Silvia López"
-          />
+          <img src={Logo} alt="Silvia López" />
         </h1>
+        <div className="sign-in__content-tabs">
+          <Tabs type="card">
+            <TabPane tab={<span>Entrar</span>} key="1">
+              Componente LoginForm
+            </TabPane>
+            <TabPane tab={<span>Nuevo usuario</span>} key="2">
+              <RegisterForm />
+            </TabPane>
+          </Tabs>
+        </div>
       </Content>
     </Layout>
   );
